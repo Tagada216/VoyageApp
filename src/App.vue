62edc9ef9d5e803c34ec7c48b69015b34ef98f5a
Nicolas Delcourt <nicolasdelcourt10@gmail.com>
2021-06-23 15:32:33 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Voyage </p>
+    <div>
+      <p>
+        Rechercher : <input v-model="country">
+      </p>
+      <button @click="getOneCountry(country)">Rechercher</button>
+    </div>
+    <div v-for="country in listCountries " :key="country.name">
+      <countries-list :country="country.translations.fr" :getDetails="getDetails" :name="country.name"></countries-list>
+    </div>
+<modal name="detail-modal">
+    <div v-if="oneCountry">
+      <p>
+        Pays : {{oneCountry.translations.fr}}
+      </p>
+      <br>
+      <p>
+        Capitale : {{oneCountry.capital}}
+      </p>
+    </div>
+</modal>
+<modal name="one-modal">
+    <div v-if="oneCountry">
+      <p>
+        Pays : {{oneCountry.translations.fr}}
+      </p>
+      <br>
+      <p>
+        Capitale : {{oneCountry.capital}}
+      </p>
+    </div>
+</modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CountriesList from './components/countriesList.vue'
+import axios from "axios";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CountriesList
+  },
+  data(){
+    return{
+      listCountries: null,
+      oneCountry: null
+    }
+  },
+  mounted(){
+    this.getAllCoutries()
+    console.log("tab", this.listCountries)
+  },
+  methods:{
+    getAllCoutries(){
+      axios.get("https://restcountries.eu/rest/v2/all").then(resp =>{
+        this.listCountries = resp.data
+      }).catch(error => console.log(error))
+    },
+    getDetails(name){
+      this.$modal.show('detail-modal');
+      axios.get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`).then(resp =>{
+        this.oneCountry = resp.data[0]
+      }).catch(error => console.log(error))     
+    },
+    getOneCountry(name){
+       this.$modal.show('detail-modal');
+        axios.get(`https://restcountries.eu/rest/v2/name/${name}`).then(resp =>{
+        this.oneCountry = resp.data[0]
+      }).catch(error => console.log(error))   
+    }
   }
 }
 </script>
---
+++ src/components/countriesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div> 
+      <p @click="getDetails(name)">{{country}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+name:"CountriesList",
+props:[
+    "country",
+    "getDetails",
+    "name"
+]
+}
+</script>
+
+<style>
+
+</style>
